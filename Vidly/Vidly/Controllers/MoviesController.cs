82361b4c2160db1dfae091ab8bbe7d0201f2ceda
Jiayuan <jiahuan.h@ciktel.com>
2018-06-22 17:27:11 -0400

    movie edit

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -42,61 +42,45 @@ namespace Vidly.Controllers
             return View(movie);
         }
 
-        private IEnumerable<Movie> GetMovies()
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
         {
-            return new List<Movie>
+            if (movie.Id == 0)
+                _context.Movies.Add(movie);
+            else
             {
-                new Movie { Id = 1, Name = "Shrek" },
-                new Movie { Id = 2, Name = "Wall-e" }
-            };
+                var movieInDb = _context.Movies
+                    .Single(m => m.Id == movie.Id);
+                // Mapper.Map(customer, customerInDb);
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.DateAdded = movie.DateAdded;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+
+                // not recommanded: security problem
+                TryUpdateModel(movieInDb);
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Movies");
         }
 
-        // GET: Movies/Random
-        //public ActionResult Random()
-        //{
-        //    var movie = new Movie() { Name = "Vivian" };
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies
+                .SingleOrDefault(m => m.Id == id);
 
-        //    var customers = new List<Customer>
-        //    {
-        //        new Customer { Name = "Customer 1"},
-        //        new Customer { Name = "Customer 2"}
-        //    };
+            if (movie == null)
+                return HttpNotFound();
 
-        //    var viewModel = new RandomMovieViewModel
-        //    {
-        //        Movie = movie,
-        //        Customers = customers
-        //    };
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
 
-        //    //ViewData["Movie"] = movie;
-
-        //    //ViewBag.Movie = movie;
-
-        //    //var viewResult = new ViewResult();
-
-        //    return View(viewModel);
-        //}
-
-        //public ActionResult Edit(int movieId)
-        //{
-        //    return Content("movieId=" + movieId);
-        //}
-
-        // movies
-        //public ActionResult Index(int? pageIndex, string sortBy)
-        //{
-        //    if (!pageIndex.HasValue)
-        //        pageIndex = 1;
-
-        //    if (String.IsNullOrWhiteSpace(sortBy))
-        //        sortBy = "Name";
-
-        //    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        //}
-        //[Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
-        //public ActionResult ByReleaseDate(int year, int month)
-        //{
-        //    return Content(year + "/" + month);
-        //}
+            return View("MovieForm", viewModel);
+        }
     }
 }
